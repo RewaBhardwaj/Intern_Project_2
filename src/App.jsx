7858dfd2fbe2df4d17d9a8f1b79d2b3ba4fbb0a3
RewaBhardwaj <rewa1683.be20@chitkara.edu.in>
2024-01-25 03:20:17 -0800

    Applying css module and bootstarp css together

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import FoodList from './Component/FoodList'
+import "./App.css"
 export default function App() {
   
     const fooditems= [ 
-      // "Pizza","Pink Pasta","Burger", "Fries", "Noodles"
+       "Pizza","Pink Pasta","Burger", "Fries", "Noodles"
     ];
       return <FoodList item={fooditems} />;
     
